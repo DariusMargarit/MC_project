d6205034f55dc2febf16abce2a51aaf04afaae06
DariusMargarit <margaritdarius89@yahoo.com>
2023-10-25 19:50:07 +0300

    Added Bridge class

---
+++ GameLib/Bridge.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "IColumn.h"
+
+enum class EPlayer {
+	FirstPlayer,
+	SecondPlayer
+};
+
+class Bridge {
+private:
+	EPlayer m_Player;
+	IColumn* m_FirstColumn, m_SecondColumn;
+};
