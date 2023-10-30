da98472744b9ba1da8de63fad79a779a471cf052
DariusMargarit <margaritdarius89@yahoo.com>
2023-10-30 11:39:30 +0200

    Board destructor

--- GameLib/Board.cpp
+++ GameLib/Board.cpp
@@ -3,3 +3,12 @@
 Board::Board() {
 
 }
+
+Board::~Board() {
+	for (int index = 0; index < m_Columns.size(); index++) {
+		delete m_Columns[index];
+	}
+	for (int index = 0; index < matrix.size(); index++) {
+		delete matrix[index];
+	}
+}
