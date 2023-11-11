3cb1562d950f49a566affb4d359da84bcea1c46b
Ivan-Cosmin <ivancosmin220503@gmail.com>
2023-11-11 11:38:17 +0200

    Move Semantics Column class

--- GameLib/Column.cpp
+++ GameLib/Column.cpp
@@ -12,6 +12,12 @@ Column::Column(const Column& otherColumn)
 
 }
 
+Column::Column(Column&& other) noexcept
+	:m_player{other.m_player}
+{
+	other.m_player = nullptr;
+}
+
 IPlayer* Column::GetPlayer() const
 {
 	return m_player;
@@ -21,5 +27,15 @@ Column& Column::operator=(const Column& otherColumn)
 {
 	if (this->GetPlayer() != otherColumn.GetPlayer())
 		this->m_player = otherColumn.GetPlayer();
+	return *this;
+}
+
+Column& Column::operator=(Column&& other) noexcept
+{
+	if (this == &other)
+		return *this;
+	m_player = other.m_player;
+
+	other.m_player = nullptr;
 	return *this;
 }
--- GameLib/Column.cppm
+++ GameLib/Column.cppm
@@ -7,8 +7,9 @@ export class Column : public IColumn
 public:
 	Column(IPlayer* player);
 	Column(const Column& other);
-
-	Column& operator=(const Column&);
+	Column(Column&& other) noexcept;
+	Column& operator=(const Column& other);
+	Column& operator=(Column&& other) noexcept;
 
 	IPlayer* GetPlayer() const override;
 
