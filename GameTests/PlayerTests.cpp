20cf3b1b84dd5a0d7eb13ee8ee840126381dfd52
codrutamiron <148007994+codrutamiron@users.noreply.github.com>
2024-01-13 19:32:41 +0200

    PlayerTests added

--- GameTests/PlayerTests.cpp
+++ GameTests/PlayerTests.cpp
@@ -21,3 +21,57 @@ protected:
 	IGamePtr m_game;
 
 };
+
+TEST_F(PlayerTests, GetDefaultColorPlayer1Test) {
+	// Perform the action
+
+	EColor resultColor = m_game->GetFirstPlayer()->GetColor();
+
+	// Verify the result
+	EXPECT_EQ(resultColor, EColor::Blue);
+}
+
+TEST_F(PlayerTests, GetDefaultColorPlayer2Test) {
+	// Perform the action
+
+	EColor resultColor = m_game->GetSecondPlayer()->GetColor();
+
+	// Verify the result
+	EXPECT_EQ(resultColor, EColor::Red);
+}
+
+TEST_F(PlayerTests, GetDefaultNamePlayer1Test) {
+	// Perform the action
+	std::string_view resultName = m_game->GetFirstPlayer()->GetName();
+
+	// Verify the result
+	EXPECT_EQ(resultName, "Player1");
+}
+
+TEST_F(PlayerTests, GetDefaultNamePlayer2Test) {
+	// Perform the action
+	std::string_view resultName = m_game->GetSecondPlayer()->GetName();
+
+	// Verify the result
+	EXPECT_EQ(resultName, "Player2");
+}
+
+TEST_F(PlayerTests, SetColorTest) {
+	// Perform the action
+	m_game->GetFirstPlayer()->SetColor(EColor::Blue);
+	m_game->GetSecondPlayer()->SetColor(EColor::Red);
+	// Verify the result
+	EXPECT_EQ(m_game->GetFirstPlayer()->GetColor(), EColor::Blue);
+	EXPECT_EQ(m_game->GetSecondPlayer()->GetColor(), EColor::Red);
+}
+
+
+TEST_F(PlayerTests, SetNameTest) {
+	// Perform the action
+	m_game->GetFirstPlayer()->SetName("NewPlayer1Name");
+	m_game->GetSecondPlayer()->SetName("NewPlayer2Name");
+
+	// Verify the result
+	EXPECT_EQ(m_game->GetFirstPlayer()->GetName(), "NewPlayer1Name");
+	EXPECT_EQ(m_game->GetSecondPlayer()->GetName(), "NewPlayer2Name");
+}
